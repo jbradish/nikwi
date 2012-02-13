3071745a03d0b4335c54f7ce839ecc6da25e38b2
Kostas Michalopoulos <badsector@runtimelegend.com>
2012-02-13 14:18:10 +0200

    Added make-linux-release.sh script

---
+++ make-linux-release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+make clean
+CFLAGS=-O3 make all
+
+TGT=nikwideluxe-linux-`uname -p`
+
+rm -fr $TGT
+mkdir $TGT
+
+cp -R manual license.txt nikwi.ico nikwi justdata.up $TGT
+strip --strip-all $TGT/nikwi
+tar cJvf $TGT.xz $TGT
+
+echo Done
+
